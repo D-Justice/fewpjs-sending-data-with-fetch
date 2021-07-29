5b9fa9c93bb91bed78bf3ace9fb7ee6df7534156
Damian <justice.damian96@gmail.com>
2021-07-29 16:33:34 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+let submitData = (userName, userEmail) => {
+    let configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': userName,
+            'email': userEmail
+        })
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(object => {
+        let userID = document.createElement('p');
+        userID.innerHTML = object.id;
+        document.body.appendChild(userID);
+        
+    })
+    .catch(function(error) {
+        let bad = document.createElement('p')
+        bad.innerHTML = error
+        document.body.appendChild(bad);
+    })
+}
